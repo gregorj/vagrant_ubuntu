c34479735a1fb094e5c983645f53089eea9bc1c4
Slade Baumann <baumann@us.ibm.com>
2017-01-30 10:44:41 -0600

    fix git vars

--- git_variables.sh
+++ git_variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Update these variables to set up your git config
-EMAIL="baumann@us.ibm.com"
-NAME="Slade Baumann"
+EMAIL=""
+NAME=""
 # If you do not remember your Gerrit user name go to the settings page on
 # gerrit to check it out (it is not your email address).
-GERRIT_USERNAME="baumann"
+GERRIT_USERNAME=""
 DEFAULT_EDITOR="vim"
