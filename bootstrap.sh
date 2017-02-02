265a79053ea46eb7df2439174d8a29b490d7b691
Slade Baumann <baumann@us.ibm.com>
2017-02-02 16:20:41 -0600

    added vim options and bash_profile beautifiers

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,28 @@ for key in `ls /vagrant/keys/`; do
     chmod 600 ~/.ssh/$key
 done
 
+# Move colors folder to .vim dir for vim colorschemes
+mkdir -p ~/.vim/colors
+for colorscheme in `ls /vagrant/colorschemes_vim/`; do
+    cp /vagrant/colorschemes_vim/$colorscheme ~/.vim/colors
+done
+
 # vim setup
 cat > ~/.vimrc << EOF
-set number
+set number                                                                
+set cursorline
+set showmatch
+colorscheme molokai
+syntax on
+EOF
+
+# colorize terminal for macosx
+cat > ~/.bash_profile << EOF
+# Command line fixes
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+alias ls='ls -GFh'
 EOF
 
 # setup git config
